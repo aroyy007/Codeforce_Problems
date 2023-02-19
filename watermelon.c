25e3636613454263489b156daff46856e5fbc460
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-02-20 01:42:52 +0600

    4A-watermelon

---
+++ watermelon.c
@@ -0,0 +1,13 @@
+//watermelon
+#include <stdio.h>
+
+int main() {
+    int w;
+    scanf("%d", &w);
+    if (w >= 4 && w % 2 == 0) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+    return 0;
+}
