f45352a5b90ee514a673ad5fbf7833f0979abe0d
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-05-18 00:28:39 +0600

    watermelon.cpp

---
+++ watermelon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w;
+    cin >> w;
+
+    if (w >= 4 && w % 2 == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
+
