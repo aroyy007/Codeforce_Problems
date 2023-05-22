c21ac0febba62b970ad529a96ffa0a80cb31f425
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-05-23 01:38:04 +0600

    the_brothers.cpp

---
+++ the_brothers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string f1,f2,s1,s2;
+    cin>>f1>>s1;
+    cin>>f1>>s2;
+    if(s1==s2)
+    {
+        cout<<"ARE Brothers"<<endl;
+    }
+    else
+    {
+        cout<<"NOT"<<endl;
+    }
+}
