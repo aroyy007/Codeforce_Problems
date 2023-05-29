186e4f68cd5caf24aea1687e69ab82ecbdbd59d3
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-05-29 22:30:04 +0600

    fixed_password.cpp

---
+++ fixed_password.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n==1999)
+        {
+            cout<<"Correct"<<endl;
+            break;
+        }
+        else
+        {
+            cout<<"Wrong"<<endl;
+        }
+    }
+}
