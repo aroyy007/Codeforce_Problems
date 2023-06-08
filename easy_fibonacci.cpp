833c9f33fdbe3ea4b4d729182270b6bf34e5df0a
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-06-08 22:01:30 +0600

    fibonacci series to n terms

---
+++ easy_fibonacci.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    a[0]=0;
+    a[1]=1;
+    for(int i=2;i<n;i++)
+    {
+        a[i]=a[i-1]+a[i-2];
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
