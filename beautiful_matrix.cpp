d61d73acde390cfff14a24b2b286d22ebcee543b
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-06-10 00:16:50 +0600

    beautiful_matrix

---
+++ beautiful_matrix.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x=0;
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            cin>>x;
+            if(x==1)
+            {
+                cout<<abs(i-2)+abs(j-2)<<endl;
+            }
+        }
+    }
+    return 0;
+}
