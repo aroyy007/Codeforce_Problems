696aa39cd0f11e504a28e9733b5e2f57830a41c7
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-06-19 02:35:18 +0600

    create_a_new_string.cpp

---
+++ create_a_new_string.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    string t;
+    cin>>t;
+    int a=s.size();
+    
+    int b=t.size();
+    cout<<a<<" "<<b<<endl;
+    cout<<s<<" "<<t<<endl;
+    return 0;
+}
