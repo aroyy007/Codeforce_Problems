8df345942510d04380c53f44c9addfe549834fd6
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-07-24 01:05:58 +0600

    onefull_pairs.cpp

---
+++ onefull_pairs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin>>a>>b;
+	if((a+b+(a*b))==111)
+	{
+	    cout<<"YES"<<endl;
+	}
+	else
+	{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
