a0b0a34a36f63f269201f3bb0708ddc0d3b30bca
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-08-12 01:37:53 +0600

    remove_smallest.cpp

---
+++ remove_smallest.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        bool flag = true;
+        for (int i = 1; i < n; i++)
+        {
+            if ((a[i] - a[i-1]) > 1)
+            {
+                flag = false;
+                break;
+            }
+            
+        }
+        if (flag)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
