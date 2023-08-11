37ba73178a3794a857f62f51f7a0bc5060989da7
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-08-12 02:03:59 +0600

    Moore's_law.cpp

---
+++ Moore's_law.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n,t;
+    cin>>n>>t;
+    double ans = n*pow(1.000000011,t);
+    cout<<fixed<<setprecision(9)<<ans<<endl;
+}
