515767c28fbc8e198700691c37410d0488deefee
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-09-05 19:00:49 +0600

    A+B=?_1772A

---
+++ A+B=?.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        char s;
+        cin >> a >> s >> b;
+        cout << a + b << endl;
+        
+    }
+    return 0;
+}
