8ef66be0e5b36878fb57bde9a99120c6e9a811bd
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-09-10 12:20:16 +0600

    Ehab_and_GCD_1325A

---
+++ Ehab_and_GCD.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        cout << 1 << " " << x - 1 << endl;
+    }
+}
