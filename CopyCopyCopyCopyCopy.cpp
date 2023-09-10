970c32fe89d9f605eeb1be51a6aee68cfa438d65
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-09-10 22:09:12 +0600

    CopyCopyCopyCopyCopy_1325B

---
+++ CopyCopyCopyCopyCopy.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n,c=0;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        sort(a, a + n);
+
+        for(int i=0;i<n-1;i++)
+        {
+            if(a[i]!=a[i+1])
+            {
+                c++;
+            }
+        }
+        cout<<c+1<<endl;
+    }
+}
