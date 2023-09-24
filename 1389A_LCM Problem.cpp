60d891d2ce9ff1a1114900418de91e28f491ff14
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-09-25 01:32:56 +0600

    1389A_LCM Problem

---
+++ 1389A_LCM Problem.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int l, r;
+        cin >> l >> r;
+        if (l * 2 > r)
+        {
+            cout << "-1 -1" << endl;
+        }
+        else
+        {
+            cout << l << " " << l * 2 << endl;
+        }
+    }
+
+    return 0;
+}
