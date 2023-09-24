5d8528c9f68b4b6a388e10dc7eb2d7496f566c4e
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-09-25 02:10:56 +0600

    1360A_Minimal Square

---
+++ 1360A_Minimal Square.cpp
@@ -0,0 +1,108 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+#define dbg(args...)            \
+    do                          \
+    {                           \
+        cerr << #args << " : "; \
+        debug(args);            \
+    } while (0)
+void debug()
+{
+    cerr << endl;
+}
+template <typename T, typename... hello>
+void debug(T arg, const hello &...rest)
+{
+    cerr << arg << ' ';
+    debug(rest...);
+}
+
+ll gcd(ll a, ll b)
+{
+    return __gcd(a, b);
+}
+ll lcm(ll a, ll b)
+{
+    return a * (b / gcd(a, b));
+}
+
+int diff(int a, int b)
+{
+    return b - a;
+}
+
+int main()
+{
+    optimize();
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        // if(a<=(2*b))
+        // {
+        //     cout<<(a*a)<<endl;
+        // }
+        // else
+        if (a == b)
+        {
+            cout << (a * a * 4) << endl;
+        }
+        // else if(a>b)
+        // {
+        //     cout<<(a*a)<<endl;
+        // }
+        else
+        {
+            int ar = min(max(a * 2, b), max(a, b * 2));
+            cout << ar * ar << endl;
+        }
+    }
+
+    return 0;
+}
