3f402adf181cc57b8239e24cd7d899cb0b8f9033
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-10-23 20:09:51 +0600

    104679B_Even_Out

---
+++ 104679B_Even_Out.cpp
@@ -0,0 +1,191 @@
+// Haloom
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+
+#define optimize()              \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+#define fraction()              \
+  cout.unsetf(ios::floatfield); \
+  cout.precision(10);           \
+  cout.setf(ios::fixed, ios::floatfield);
+#define file()                      \
+  freopen("input.txt", "r", stdin); \
+  freopen("output.txt", "w", stdout);
+
+#define dbg(args...)        \
+  do                        \
+  {                         \
+    cerr << #args << " : "; \
+    debug(args);            \
+  } while (0)
+void debug()
+{
+  cerr << endl;
+}
+template <typename T, typename... hello>
+void debug(T arg, const hello &...rest)
+{
+  cerr << arg << ' ';
+  debug(rest...);
+}
+
+ll gcd(ll a, ll b)
+{
+  return __gcd(a, b);
+}
+ll lcm(ll a, ll b)
+{
+  return a * (b / gcd(a, b));
+}
+
+int diff(int a, int b)
+{
+  return b - a;
+}
+
+bool check_palindrome(string s)
+{
+  string a = s;
+  reverse(a.begin(), a.end());
+  if (s == a)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool check_square(int n)
+{
+  int s = sqrt(n);
+  return s * s == n;
+}
+
+bool check_cube(int n)
+{
+  int s = round(cbrt(n));
+  return s * s * s == n;
+}
+
+bool check_prime(int n)
+{
+  if (n == 1)
+  {
+    return false;
+  }
+  for (int i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool isFeasible(const vector<long long> &X, double meetingPoint)
+{
+  double maxDistance = 0;
+  for (long long xi : X)
+  {
+    maxDistance = max(maxDistance, abs(xi - meetingPoint));
+  }
+  return maxDistance <= meetingPoint;
+}
+
+int sum_d(int n)
+{
+  int sum = 0;
+  while (n > 0)
+  {
+    sum += n % 10;
+    n /= 10;
+  }
+  return sum;
+}
+
+int main()
+{
+  optimize();
+
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n, even = 0, odd = 0;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+      cin >> a[i];
+      if (a[i] % 2 == 0)
+      {
+        even++;
+      }
+      else
+      {
+        odd++;
+      }
+    }
+
+    int sum = accumulate(a.begin(), a.end(), 0);
+
+    // if (even == n)
+    // {
+    //   cout << n << endl;
+    // }
+    // else if (odd % 2 == 0)
+    // {
+    //   cout << n - 1 << endl;
+    // }
+    // else
+    // {
+    //   cout << n - 2 << endl;
+    // }
+
+    if (sum % 2 == 0)
+    {
+      cout << n << endl;
+    }
+    else
+    {
+      cout << 0 << endl;
+    }
+  }
+  return 0;
+}
