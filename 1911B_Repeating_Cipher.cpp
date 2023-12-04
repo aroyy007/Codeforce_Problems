ce1d39a8f0e4378f4c782a1f100ca1f5eaa0253e
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-12-04 20:29:41 +0600

    1911B_Repeating_Cipher

---
+++ 1911B_Repeating_Cipher.cpp
@@ -0,0 +1,191 @@
+//kotlin version
+/*
+import kotlin.math.*
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val t = readLine()!!
+
+    var s = ""
+    var i = 0
+
+    while (i < n) {
+        s += t[i]
+        i += s.length
+    }
+
+    println(s)
+}
+*/
+
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+#define dbg(args...)            \
+    do                          \
+    {                           \
+        cerr << #args << " : "; \
+        debug(args);            \
+    } while (0)
+void debug()
+{
+    cerr << endl;
+}
+template <typename T, typename... hello>
+void debug(T arg, const hello &...rest)
+{
+    cerr << arg << ' ';
+    debug(rest...);
+}
+
+ll gcd(ll a, ll b)
+{
+    return __gcd(a, b);
+}
+ll lcm(ll a, ll b)
+{
+    return a * (b / gcd(a, b));
+}
+
+int diff(int a, int b)
+{
+    return b - a;
+}
+
+bool check_palindrome(string s)
+{
+    string a = s;
+    reverse(a.begin(), a.end());
+    if (s == a)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool check_square(int n)
+{
+    int s = sqrt(n);
+    return s * s == n;
+}
+
+bool check_cube(int n)
+{
+    int s = round(cbrt(n));
+    return s * s * s == n;
+}
+
+bool check_prime(int n)
+{
+    if (n == 1)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isComposite(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+
+    for (int i = 2; i <= n / 2; ++i)
+    {
+        if (n % i == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int sum_d(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    optimize();
+
+    int n;
+    cin >> n;
+
+    string t;
+    cin >> t;
+
+    string s;
+    int i = 0;
+
+    while (i < n)
+    {
+        s += t[i];
+        i += s.length();
+    }
+    cout << s << endl;
+
+    return 0;
+}
