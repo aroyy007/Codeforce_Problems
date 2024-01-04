a3ea56c3ee58d51e8b1e15949916b833936b08f5
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-01-04 22:55:22 +0600

    1913A_Rating_Increase

---
+++ 1913A_Rating_Increase.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+using namespace std;
+
+int main()
+{
+    optimize();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        bool found = false;
+
+        for (int i = 1; i < s.size(); i++)
+        {
+            string t1 = s.substr(0, i);
+            string t2 = s.substr(i);
+
+            if (t1[0] == '0' || t2[0] == '0')
+            {
+                continue;
+            }
+
+            int a = stoi(t1);
+            int b = stoi(t2);
+
+            if (b > a)
+            {
+                cout << t1 << " " << t2 << endl;
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            cout << -1 << endl;
+        }
+    }
+    return 0;
+}
