d59eb38a2b243e8987a78b3447b67ab413980e8a
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-02-07 11:57:56 +0600

    1927B_Following_the_String

---
+++ 1927B_Following_the_String.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+using namespace std;
+
+int main()
+{
+    optimize();
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        ain(a, n);
+
+        map<char, int> b;
+        string ans = "";
+        for (int i = 0; i < n; ++i)
+        {
+            for (char c = 'a'; c <= 'z'; ++c)
+            {
+                if (b[c] == a[i])
+                {
+                    ans += c;
+                    b[c]++;
+                    break;
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+    
+
+    return 0;
+}
