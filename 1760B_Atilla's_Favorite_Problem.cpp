3da6a8a2867d1fd8bc3c664db2b1025ade855cd2
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-02-11 01:23:46 +0600

    1760B_Atilla's_Favorite_Problem

---
+++ 1760B_Atilla's_Favorite_Problem.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+int main()
+{
+    optimize();
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        sort(s.begin(), s.end());
+
+        char c = s[n - 1];
+        int cnt = 0;
+
+        for (char i = 'a'; i <= 'z'; i++)
+        {
+            cnt++;
+            if (i == c)
+            {
+                cnt++;
+                break;
+            }
+        }
+        cout << cnt - 1 << endl;
+    }
+
+    return 0;
+}
