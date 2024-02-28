efb1f5afee741dc181859d49fd971a582758c6b9
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-02-28 19:09:28 +0600

    1933B_Turtle_Math:_Fast_Three_Task

---
+++ 1933B_Turtle_Math:_Fast_Three_Task.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+const int N = 2 * 1e5 + 10;
+int main()
+{
+    optimize();
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        ain(a, n);
+
+        ll sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += a[i];
+        }
+        // cout << sum << endl;
+
+        if (sum % 3 == 0)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+        if (sum % 3 == 2)
+        {
+            cout << 1 << endl;
+            continue;
+        }
+
+        if (sum % 3 == 1)
+        {
+            bool flag = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] % 3 == 1)
+                {
+                    cout << 1 << endl;
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                cout << 2 << endl;
+            }
+        }
+    }
+
+    return 0;
+}
