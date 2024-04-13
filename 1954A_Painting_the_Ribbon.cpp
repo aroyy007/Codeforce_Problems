d1900daa83413318734b834ab7345403179b8da6
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-04-13 11:31:32 +0600

    1954A_Painting_the_Ribbon

---
+++ 1954A_Painting_the_Ribbon.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+const int N = 2 * 1e5 + 10;
+int main()
+{
+    optimize();
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n,m,k;
+        cin>>n>>m>>k;
+
+        int x = ceil((n*1.0)/m);
+
+        if((n-x)<=k)
+        {
+            con;
+        }
+        else
+        {
+            coy;
+        }
+    }
+
+    return 0;
+}
