e36e5778d733c2fde39e5e92f34a30eaa2e1f22a
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-05-11 01:48:40 +0600

    1971D_Binary_Cut

---
+++ 1971D_Binary_Cut.cpp
@@ -0,0 +1,192 @@
+/*
+
+____ __ __ __ __ __ ___                             ___ __ __ __ __ __ ____
+ `-._:  .:'   `:::  .:\           |\__/|           /::  .:'   `:::  .:.-'
+     \      :          \          |:   |          /         :       /
+      \     ::    .     `-_______/ ::   \_______-'   .      ::   . /
+       |  :   :: ::'  :   :: ::'  :   :: ::'      :: ::'  :   :: :|
+       |     ;::         ;::         ;::         ;::         ;::  |
+       |  .:'   `:::  .:'   `:::  .:'   `:::  .:'   `:::  .:'   `:|
+       /     :           :           :           :           :    \
+      /______::_____     ::    .     ::    .     ::   _____._::____\
+                    `----._:: ::'  :   :: ::'  _.----'
+                           `--.       ;::  .--'
+                               `-. .:'  .-'
+                                  \    /
+                                   \  /
+                                    \/
+*/
+
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+ll gcd(ll a, ll b)
+{
+    return __gcd(a, b);
+}
+ll lcm(ll a, ll b)
+{
+    return a * (b / gcd(a, b));
+}
+
+int diff(int a, int b)
+{
+    return b - a;
+}
+
+bool check_palindrome(string s)
+{
+    string a = s;
+    reverse(a.begin(), a.end());
+    if (s == a)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool check_square(ll n)
+{
+    ll s = sqrt(n);
+    return s * s == n;
+}
+
+bool check_cube(int n)
+{
+    int s = round(cbrt(n));
+    return s * s * s == n;
+}
+
+bool check_prime(int n)
+{
+    if (n == 1)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isComposite(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+
+    for (int i = 2; i <= n / 2; ++i)
+    {
+        if (n % i == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int sum_d(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    optimize();
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        // int n = stoi(s);
+
+        vector<ll> v;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            int n = s[i] - '0';
+            v.push_back(n);
+        }
+
+        int c = 0;
+        int d = 0;
+        int ans = 0;
+
+        for (int i = 0; i < v.size() - 1; i++)
+        {
+            if (v[i] == 0 && v[i + 1] == 1)
+            {
+                c++;
+            }
+            if (v[i] == 1 && v[i + 1] == 0)
+            {
+                d++;
+            }
+        }
+
+        ans = d + max(0, c - 1) + 1;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
