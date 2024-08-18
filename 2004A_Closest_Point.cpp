0d77edc147d39d25ab2b6ebe8b6dc9fec043736a
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-08-18 12:55:49 +0600

    2004A_Closest_Point

---
+++ 2004A_Closest_Point.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+const int N = 2 * 1e5 + 10;
+int main()
+{
+    optimize();
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        ain(a, n);
+
+        if (n == 2 && abs(a[0] - a[1]) > 1)
+        {
+            coy;
+        }
+        else
+        {
+            con;
+        }
+    }
+
+    return 0;
+}
