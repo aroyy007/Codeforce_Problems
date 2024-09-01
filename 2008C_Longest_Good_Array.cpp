2292471a3aa6b3ffc0bb1c604e6c9fa0d53ec118
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-09-02 00:56:30 +0600

    2008C_Longest_Good_Array

---
+++ 2008C_Longest_Good_Array.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+#include <bits/stdc++.h>
+
+#include <algorithm>
+
+#include <vector>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+using namespace std;
+
+int main()
+{
+    optimize();
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll l, r;
+        cin >> l >> r;
+
+        ll idx = 1;
+        ll len = 0;
+        ll srt = l;
+
+        while (srt + idx <= r)
+        {
+            srt += idx;
+            idx++;
+            len++;
+        }
+
+        cout << len + 1 << endl;
+    }
+    return 0;
+}
