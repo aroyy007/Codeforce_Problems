a883e57b119b20120fab3d7a022916e3c1ffa251
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-09-02 00:52:50 +0600

    2008A_Sakurako's_Exam

---
+++ 2008A_Sakurako's_Exam.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+#include <bits/stdc++.h>
+
+#include <algorithm>
+
+#include <vector>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+using namespace std;
+
+int main()
+{
+    optimize();
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        int totalSum = a + 2 * b;
+
+        if (totalSum % 2 != 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            int halfSum = totalSum / 2;
+            if (halfSum <= a + 2 * b && (halfSum % 2 <= a))
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+        }
+    }
+    return 0;
+}
