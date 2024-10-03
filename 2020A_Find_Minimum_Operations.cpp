fcdae3bb9a105f09953c26845b58fa78b5c11148
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-10-03 15:08:07 +0600

    2020A_Find_Minimum_Operations

---
+++ 2020A_Find_Minimum_Operations.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+#include <bits/stdc++.h>
+
+#include <algorithm>
+
+#include <vector>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+using namespace std;
+
+int main()
+{
+    optimize();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        if (k == 1)
+        {
+            cout << n << endl;
+            continue;
+        }
+
+        int ans = 0;
+
+        while (n > 0)
+        {
+            int temp = n % k;
+            if (temp)
+            {
+                ans += temp;
+            }
+            n /= k;
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
