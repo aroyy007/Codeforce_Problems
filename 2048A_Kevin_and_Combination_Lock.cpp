471a612290cd76bb4caf8258f78f0c5755f41bf3
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-12-20 10:52:08 +0600

    2048A_Kevin_and_Combination_Lock

---
+++ 2048A_Kevin_and_Combination_Lock.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+void solve() {
+    
+    int n;
+    cin >> n;
+
+    if(n%33==0){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+}
+
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
