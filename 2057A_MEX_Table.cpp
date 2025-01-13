f47b8e253c2806773d53b86c8c74cc9cc29cf3f7
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2025-01-13 13:08:11 +0600

    2057A_MEX_Table

---
+++ 2057A_MEX_Table.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define ll long long
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+    cout<<max(n,m)+1<<endl;
+}
+
+
+
+int main() {
+    optimize();
+    ll t,i=1;
+    cin>>t;
+    while(t--){
+        //cout<<"case "<<i<<": ";
+        solve();
+       //i++;
+    }
+    return 0;
+}
+
+
+    
